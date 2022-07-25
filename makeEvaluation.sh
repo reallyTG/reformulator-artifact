271fcfe2215313b78785e7266b80b262170f4e4b
Alexi Turcotte <alexi.s.f.turcotte@gmail.com>
2022-07-25 15:42:04 -0400

    Update makeEvaluation.sh

--- makeEvaluation.sh
+++ makeEvaluation.sh
@@ -2,5 +2,8 @@
 
 # Make some directories.
 mkdir -p /home/evaluation/case-studies
+mkdir /home/evaluation/query-results
+mkdir /home/evaluation/QLDBs
 
+# Bring scripts over.
 cp -r /scripts /home/evaluation/scripts
